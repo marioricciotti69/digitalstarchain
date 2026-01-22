fbd27b47b6bf51f82db6f80b7fde027816fc7007
marioricciotti69 <marioricciotti69@gmail.com>
2026-01-22 11:42:55 +0100

    Create contactInfo.ts

---
+++ app/contact/contactInfo.ts
@@ -0,0 +1,9 @@
+export const contactInfo = {
+  companyName: "Digital Star Chain",
+  email: "hello@digitalstarchain.online",
+  phone: "+971503523081",
+  addressLine: "HONG KONG",
+  cityCountry: "HONG KONG",
+  hours: "Mon–Fri",
+  formspreeEndpoint: "" // <-- qui metterai il link Formspree
+};
